9b6cb201eebb38ae1aa231fdca6fdd61e7cc53fc
juliana <julianasantostav@gmail.com>
2024-07-12 15:24:05 -0300

    feat: create project

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System; 
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Soma();
+
+        }
+
+        static void Soma()
+        {
+            Console.Clear();
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine()); 
+
+            Console.WriteLine("Segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine()); 
+
+            Console.WriteLine("");
+
+            float resultado = v1 + v2;
+            Console.WriteLine("O resultado da soma é " + resultado); 
+        }
+    }
+}
