d6c119837e831175951a827db61f493e29247c5a
juliana <julianasantostav@gmail.com>
2024-07-17 13:07:09 -0300

    feat: create exit

--- Program.cs
+++ Program.cs
@@ -18,6 +18,7 @@ namespace Calculator
             Console.WriteLine("2 - Subtração")
             Console.WriteLine("3 - Divisão")
             Console.WriteLine("4 - Multiplicação")
+            Console.WriteLine("5 - Sair")
             Console.WriteLine("-----------------")
 
             short res = short.Parse(Console.ReadLine());
@@ -28,6 +29,7 @@ namespace Calculator
                 case 2: Subtracao(); break;
                 case 3: Divisao(); break; 
                 case 4: Multiplicacao(); break;
+                case 5: System.Environment.Exit(0); break;
                 default: Menu(); break;
             }
         }
@@ -46,6 +48,7 @@ namespace Calculator
             float resultado = v1 + v2;
             Console.WriteLine("O resultado da soma é " + resultado); 
             Console.ReadKey();
+            Menu();
         }
 
         static void Subtracao()
@@ -62,6 +65,7 @@ namespace Calculator
             float resultado = v1 - v2;
             Console.WriteLine($"O resultado da subtração é {resultado}");
             Console.ReadKey();
+            Menu();
         }
 
         static void Divisao()
@@ -78,6 +82,7 @@ namespace Calculator
             float resultado = v1/v2;
             Console.WriteLine($"O resultado da divisão é {resultado}");
             Console.ReadKey();
+            Menu();
         }
 
         static void Multiplicacao()
@@ -94,6 +99,7 @@ namespace Calculator
             float resultado = v1*v2;
             Console.WriteLine($"O resultado da multiplicação é {resultado}");
             Console.ReadKey();
+            Menu();
 
         }
     }
