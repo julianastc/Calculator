57de93f0575aec0653dc4d8323d718861ec53ef7
juliana <julianasantostav@gmail.com>
2024-07-20 15:35:57 -0300

    feat: setup MostreErro

--- Program.cs
+++ Program.cs
@@ -7,41 +7,65 @@ namespace Calculator
         static void Main(string[] args)
         {
            Menu();
-
         }
 
         static void Menu()
         {
             Console.Clear(); 
-            Console.WriteLine("Selecione uma opção")
-            Console.WriteLine("1 - Soma")
-            Console.WriteLine("2 - Subtração")
-            Console.WriteLine("3 - Divisão")
-            Console.WriteLine("4 - Multiplicação")
-            Console.WriteLine("5 - Sair")
-            Console.WriteLine("-----------------")
+            Console.WriteLine("Selecione uma opção");
+            Console.WriteLine("1 - Soma");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Quociente da Divisão");
+            Console.WriteLine("4 - Resto da Divisão");
+            Console.WriteLine("5 - Multiplicação");
+            Console.WriteLine("6 - Sair");
+            Console.WriteLine("-----------------");
 
-            short res = short.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            short res; 
+            bool isValidShort = short.TryParse(input, out res);
+
+            if (!isValidShort) 
+            {
+                res = 0;
+            }
 
             switch (res)
             {
                 case 1: Soma(); break;
                 case 2: Subtracao(); break;
-                case 3: Divisao(); break; 
-                case 4: Multiplicacao(); break;
-                case 5: System.Environment.Exit(0); break;
-                default: Menu(); break;
+                case 3: DivisaoQuociente(); break;
+                case 4: DivisaoResto(); break; 
+                case 5: Multiplicacao(); break;
+                case 6: System.Environment.Exit(0); break;
+                default: MostreErro(); break;
             }
         }
 
         static void Soma()
         {
             Console.Clear();
-            Console.WriteLine("Primeiro valor: ");
-            float v1 = float.Parse(Console.ReadLine()); 
+            
+            Console.WriteLine("Digite um número: ");
+            string inputValueOne = Console.ReadLine();
+            float v1;
+            bool isValidFloatOne = float.TryParse(inputValueOne, out v1);
+            
+            if (!isValidFloatOne)
+            {
+                MostreErro();
+            }
 
-            Console.WriteLine("Segundo valor: ");
-            float v2 = float.Parse(Console.ReadLine()); 
+            Console.WriteLine("Digite outro número: ");
+            string inputValueTwo= Console.ReadLine();
+            float v2;
+            bool isValidFloatTwo = float.TryParse(inputValueTwo, out v2);
+            
+            if (!isValidFloatTwo)
+            {
+                MostreErro();
+            }
+
 
             Console.WriteLine("");
 
@@ -54,12 +78,26 @@ namespace Calculator
         static void Subtracao()
         {
             Console.Clear();
-            Console.WriteLine("Primeiro valor: ");
-            float v1 = float.Parse(Console.ReadLine()); 
+           
+            Console.WriteLine("Digite um número: ");
+            string inputValueOne = Console.ReadLine();
+            float v1;
+            bool isValidFloatOne = float.TryParse(inputValueOne, out v1);
+            
+            if (!isValidFloatOne)
+            {
+                MostreErro();
+            }
 
-            Console.WriteLine("Segundo valor: ");
-            float v2 = float.Parse(Console.ReadLine()); 
-
+            Console.WriteLine("Digite outro número: ");
+            string inputValueTwo= Console.ReadLine();
+            float v2;
+            bool isValidFloatTwo = float.TryParse(inputValueTwo, out v2);
+            
+            if (!isValidFloatTwo)
+            {
+                MostreErro();
+            }
             Console.WriteLine("");
 
             float resultado = v1 - v2;
@@ -68,19 +106,64 @@ namespace Calculator
             Menu();
         }
 
-        static void Divisao()
+        static void DivisaoQuociente()
         {
             Console.Clear();
-            Console.WriteLine("Primeiro valor: ");
-            float v1 = float.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Digite um número: ");
+            string inputValueOne = Console.ReadLine();
+            float v1;
+            bool isValidFloatOne = float.TryParse(inputValueOne, out v1);
+            
+            if (!isValidFloatOne)
+            {
+                MostreErro();
+            }
 
-            Console.WriteLine("Segundo valor: ");
-            float v2 = float.Parse(Console.ReadLine());
-
+            Console.WriteLine("Digite outro número: ");
+            string inputValueTwo= Console.ReadLine();
+            float v2;
+            bool isValidFloatTwo = float.TryParse(inputValueTwo, out v2);
+            
+            if (!isValidFloatTwo)
+            {
+                MostreErro();
+            }
             Console.WriteLine("");
             
             float resultado = v1/v2;
-            Console.WriteLine($"O resultado da divisão é {resultado}");
+            Console.WriteLine($"O quociente da divisão é {resultado}");
+            Console.ReadKey();
+            Menu();
+        }
+
+        static void DivisaoResto()
+        {
+            Console.Clear();
+            
+            Console.WriteLine("Digite um número: ");
+            string inputValueOne = Console.ReadLine();
+            float v1;
+            bool isValidFloatOne = float.TryParse(inputValueOne, out v1);
+            
+            if (!isValidFloatOne)
+            {
+                MostreErro();
+            }
+
+            Console.WriteLine("Digite outro número: ");
+            string inputValueTwo= Console.ReadLine();
+            float v2;
+            bool isValidFloatTwo = float.TryParse(inputValueTwo, out v2);
+            
+            if (!isValidFloatTwo)
+            {
+                MostreErro();
+            }
+            Console.WriteLine("");
+
+            float resultado = v1 % v2;
+            Console.WriteLine($"O resto da divisão é {resultado}");
             Console.ReadKey();
             Menu();
         }
@@ -88,12 +171,26 @@ namespace Calculator
         static void Multiplicacao()
         {
             Console.Clear();
-            Console.WriteLine("Primeiro valor: ");
-            float v1 = float.Parse(Console.ReadLine());
+           
+            Console.WriteLine("Digite um número: ");
+            string inputValueOne = Console.ReadLine();
+            float v1;
+            bool isValidFloatOne = float.TryParse(inputValueOne, out v1);
+            
+            if (!isValidFloatOne)
+            {
+                MostreErro();
+            }
 
-            Console.WriteLine("Segundo valor: ");
-            float v2 = float.Parse(Console.ReadLine());
-
+            Console.WriteLine("Digite outro número: ");
+            string inputValueTwo= Console.ReadLine();
+            float v2;
+            bool isValidFloatTwo = float.TryParse(inputValueTwo, out v2);
+            
+            if (!isValidFloatTwo)
+            {
+                MostreErro();
+            }
             Console.WriteLine(""); 
 
             float resultado = v1*v2;
@@ -101,6 +198,13 @@ namespace Calculator
             Console.ReadKey();
             Menu();
 
+        }
+
+        static void MostreErro()
+        {
+            Console.WriteLine("Caractere Inválido! Aperte enter para continuar.");
+            Console.ReadLine();
+            Menu();
         }
     }
 }
